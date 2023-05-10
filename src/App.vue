41065cdfc5392eaef4e0e513985f60091c03ab8b
Angele Garrido <anggarvil@alu.edu.gva.es>
2023-05-10 10:29:03 +0200

    Commit con Vcode

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">SUSH</RouterLink>
       </nav>
     </div>
   </header>
