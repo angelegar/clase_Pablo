e159f5d1d11b3839c3fa53a9bf02ae5bb5396f1d
Angele Garrido <anggarvil@alu.edu.gva.es>
2023-05-10 10:09:40 +0200

    mi primer commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Hola vue</h1>
+</template>
+
+<script setup>
+    import Nav from '../components/Nav.vue'
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+
+<template>
+  
+  <Angele/>
+
+    <h1>my home</h1>
+ </template>
